8d4a78b23c6ab09f7637a0a1ba26f762b08ea9e7
Thomas Garcia <tjg9265@yahoo.com>
2026-03-10 21:17:08 -0600

    Start Next.js dashboard tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <div className="relative w-0 h-0border-1-[15px] border-r-[15px] border-b-[26px] border-1-transparent border-b-black"/>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
